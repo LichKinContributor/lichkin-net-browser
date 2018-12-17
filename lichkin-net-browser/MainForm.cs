842071c015f2349811e8a76bc31c857032f33d38
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-12-17 19:52:52 +0800

    init projects

---
+++ lichkin-net-browser/MainForm.cs
@@ -0,0 +1,26 @@
+﻿using LichKin.Utils;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace lichkin_net_browser
+{
+    public partial class MainForm : Form
+    {
+        private static String CONFIG_FILE_NAME = "/opt/security/browser.json";
+        private static String ICON_FILE_NAME = "/opt/security/browser.ico";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Dictionary<String, String> config = JSONFileReader.Read(CONFIG_FILE_NAME);
+            this.Text = DictionaryUtils.GetString(config, "Title", "LichKin");
+            webBrowser.Url = new Uri(DictionaryUtils.GetString(config, "Url", "http://www.lichkin.com/"));
+            this.Icon = System.Drawing.Icon.ExtractAssociatedIcon(ICON_FILE_NAME);
+        }
+    }
+}
