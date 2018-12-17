9eb0dcbe0bbba925bb292f54c72040c7d57e413f
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-12-18 00:41:43 +0800

    全屏显示应用，右键菜单改写实现最小化、关闭功能。

--- lichkin-net-browser/MainForm.Designer.cs
+++ lichkin-net-browser/MainForm.Designer.cs
@@ -28,18 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.contextMenuStripRight = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStripRightItemMinimize = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStripRightItemClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStripRight.SuspendLayout();
             this.SuspendLayout();
             // 
             // webBrowser
             // 
+            this.webBrowser.ContextMenuStrip = this.contextMenuStripRight;
             this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.IsWebBrowserContextMenuEnabled = false;
             this.webBrowser.Location = new System.Drawing.Point(0, 0);
             this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser.Name = "webBrowser";
             this.webBrowser.Size = new System.Drawing.Size(1000, 600);
             this.webBrowser.TabIndex = 0;
+            // 
+            // contextMenuStripRight
+            // 
+            this.contextMenuStripRight.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.contextMenuStripRightItemMinimize,
+            this.contextMenuStripRightItemClose});
+            this.contextMenuStripRight.Name = "contextMenuStripRight";
+            this.contextMenuStripRight.Size = new System.Drawing.Size(113, 48);
+            // 
+            // contextMenuStripRightItemMinimize
+            // 
+            this.contextMenuStripRightItemMinimize.Name = "contextMenuStripRightItemMinimize";
+            this.contextMenuStripRightItemMinimize.Size = new System.Drawing.Size(112, 22);
+            this.contextMenuStripRightItemMinimize.Text = "最小化";
+            this.contextMenuStripRightItemMinimize.Click += new System.EventHandler(this.contextMenuStripRightItemMinimize_Click);
+            // 
+            // contextMenuStripRightItemClose
+            // 
+            this.contextMenuStripRightItemClose.Name = "contextMenuStripRightItemClose";
+            this.contextMenuStripRightItemClose.Size = new System.Drawing.Size(112, 22);
+            this.contextMenuStripRightItemClose.Text = "关闭";
+            this.contextMenuStripRightItemClose.Click += new System.EventHandler(this.contextMenuStripRightItemClose_Click);
             // 
             // MainForm
             // 
@@ -47,11 +76,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1000, 600);
             this.Controls.Add(this.webBrowser);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.Text = "LichKin";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.MainForm_Load);
+            this.contextMenuStripRight.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -59,6 +90,9 @@
         #endregion
 
         private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStripRight;
+        private System.Windows.Forms.ToolStripMenuItem contextMenuStripRightItemMinimize;
+        private System.Windows.Forms.ToolStripMenuItem contextMenuStripRightItemClose;
     }
 }
 
--- lichkin-net-browser/MainForm.cs
+++ lichkin-net-browser/MainForm.cs
@@ -5,22 +5,57 @@ using System.Windows.Forms;
 
 namespace lichkin_net_browser
 {
+    /// <summary>
+    ///     主窗体
+    /// </summary>
     public partial class MainForm : Form
     {
-        private static String CONFIG_FILE_NAME = "/opt/security/browser.json";
-        private static String ICON_FILE_NAME = "/opt/security/browser.ico";
-
+        /// <summary>
+        ///     构造方法
+        /// </summary>
         public MainForm()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        ///     主窗体加载后事件
+        /// </summary>
         private void MainForm_Load(object sender, EventArgs e)
+        {
+            // 使用配置文件初始化
+            initByConfigs();
+        }
+
+        // 配置文件
+        private static String CONFIG_FILE_NAME = "/opt/security/browser.json";
+        // 配置图标
+        private static String ICON_FILE_NAME = "/opt/security/browser.ico";
+        /// <summary>
+        ///     使用配置文件初始化
+        /// </summary>
+        private void initByConfigs()
         {
             Dictionary<String, String> config = JSONFileReader.Read(CONFIG_FILE_NAME);
             this.Text = DictionaryUtils.GetString(config, "Title", "LichKin");
             webBrowser.Url = new Uri(DictionaryUtils.GetString(config, "Url", "http://www.lichkin.com/"));
             this.Icon = System.Drawing.Icon.ExtractAssociatedIcon(ICON_FILE_NAME);
+        }
+
+        /// <summary>
+        ///     最小化
+        /// </summary>
+        private void contextMenuStripRightItemMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+        }
+
+        /// <summary>
+        ///     关闭
+        /// </summary>
+        private void contextMenuStripRightItemClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
